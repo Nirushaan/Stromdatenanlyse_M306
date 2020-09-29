6f03b48eec86135fe69d5bd22518c8a23d42068e
Brenden7788 <brendenjose2002@gmail.com>
2020-09-29 15:05:27 +0200

    Beginning Combine_esl_sdat

---
+++ src/Main/Gui/VerbrauchsGui.java
@@ -0,0 +1,11 @@
+package Main.Gui;
+
+/**
+ * short description of VerbrauchsGui
+ *
+ * @author Brenden Jose
+ * @version 1.0
+ * @since 29.09.2020
+ */
+public class VerbrauchsGui {
+}
---
+++ src/Main/Gui/ZaehlerstandGui.java
@@ -0,0 +1,11 @@
+package Main.Gui;
+
+/**
+ * short description of ZaehlerstandGui
+ *
+ * @author Brenden Jose
+ * @version 1.0
+ * @since 29.09.2020
+ */
+public class ZaehlerstandGui {
+}
