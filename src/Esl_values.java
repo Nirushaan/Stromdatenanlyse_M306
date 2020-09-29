03180793b0e670f35a82c816a6bef24701402e20
Hunter <hunterinswitzerland@gmail.com>
2020-09-29 13:42:25 +0200

    Revert "Delete Esl_values.java"
    This reverts commit 0f0828672179111f37d8d4fc1248a13162f37076.

---
+++ src/Esl_values.java
@@ -0,0 +1,20 @@
+public class Esl_values {
+    private String obis;
+    private Float value;
+
+    public String getObis() {
+        return obis;
+    }
+
+    public void setObis(String obis) {
+        this.obis = obis;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+}
